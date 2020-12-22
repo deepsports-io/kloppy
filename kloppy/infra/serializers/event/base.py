1141c312bb26ba284dbdf02b84cb5f01f209c49b
Felix Schmidt <fs@deepsports.io>
2020-12-22 17:27:52 +0100

    #78: initial attempt

--- kloppy/infra/serializers/event/base.py
+++ kloppy/infra/serializers/event/base.py
@@ -1,16 +1,28 @@
 from abc import ABC, abstractmethod
-from typing import Tuple, Dict
+from typing import Any, Dict, Tuple
 
-from kloppy.utils import Readable
 from kloppy.domain import EventDataset
+from kloppy.utils import Readable
 
 
 class EventDataSerializer(ABC):
     @abstractmethod
+    def _load_raw_events(
+        self, inputs: Dict[str, Readable], options: Dict = None
+    ) -> Any:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _parse_raw_events(
+        self, raw_events: Any, options: Dict = None
+    ) -> EventDataset:
+        raise NotImplementedError
+
     def deserialize(
         self, inputs: Dict[str, Readable], options: Dict = None
     ) -> EventDataset:
-        raise NotImplementedError
+        raw_events = self._load_raw_events(inputs, options)
+        return self._parse_raw_events(raw_events, options)
 
     @abstractmethod
     def serialize(self, dataset: EventDataset) -> Tuple[str, str]:
